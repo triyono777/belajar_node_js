3e27bac671ca642a55f4aa702faabcc8e1e834f5
Triyono <yono.tri@gmail.com>
2022-06-06 08:47:43 +0700

    add routes file

--- index.js
+++ index.js
@@ -1,23 +1,14 @@
 //import express
 import express from "express";
  
+// import router
+import Router from "./routes/routes.js";
+ 
 // init express
 const app = express();
  
-// Home route
-app.get('/', (req, res) => {
-    res.send('Welcome to Home Page');
-});
- 
-// About route
-app.get('/about', (req, res) => {
-    res.send('Welcome to About Page');
-});
- 
-// Contact route
-app.get('/contact', (req, res) => {
-    res.send('Welcome to Contact Page');
-});
+// Use Router
+app.use(Router);
  
 // listen on port
 app.listen(3000, () => console.log('Server Running at http://localhost:3000'));
